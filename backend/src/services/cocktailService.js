773586fd1e226218db832496e89d65fce9fcc5f5
Tábi István <tabi.pisti@gmail.com>
2021-09-24 17:37:26 +0200

    added service functions to backend

--- backend/src/services/cocktailService.js
+++ backend/src/services/cocktailService.js
@@ -1,5 +1,67 @@
+import fetch from 'node-fetch';
+
+import ValidationError from '../utils/ValidationError.js';
+
 const cocktailService = {
-    getCocktail() {}
+    apiUrl: `www.thecocktaildb.com/api/json/v1/${process.env.API_KEY}`,
+
+    async getCocktail(cocktailName) {
+        let endpoint;
+
+        if (!cocktailName) {
+            endpoint = `${this.apiUrl}/random.php`
+        } else {
+            endpoint = `${this.apiUrl}/search.php?s=${cocktailName}`
+        }
+
+
+        const resultFromApi = await fetch(endpoint, {
+            method: 'GET'
+        })
+
+        const responseFromApi = await resultFromApi.json();
+
+        if (responseFromApi === null) {
+            throw new ValidationError('This cocktail is not found')
+        } else {
+            const drinkList = this.processResponseFromApi(resultFromApi);
+            return drinkList
+        }
+    },
+
+    processResponseFromApi(responseFromApi) {
+        const drinkList = [];
+
+        responseFromApi.forEach((drink) => {
+            const name = drink.strDrink;
+            const ingredientsList = [];
+            const meausermentList = [];
+            const prepare = drink.strInstructions; 
+            const picture = drink.strDrinkThumb
+    
+            Object.entries(drink).forEach(([key, value]) => {
+                if (key.includes('Ingredient') && value !== null) {
+                    ingredientsList.push(value)
+                }
+    
+                if (key.includes('Measure') && value !== null) {
+                    meausermentList.push(value)
+                }
+            })
+
+            drinkList.push({
+                name,
+                ingredientsList,
+                meausermentList,
+                prepare,
+                picture
+            })
+
+        })
+
+        return drinkList
+
+    }
 }
 
 export default cocktailService;
